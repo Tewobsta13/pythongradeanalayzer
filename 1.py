b68c0d01ed33ca5050373ba95a5869052fb654e9
Tewobsta <tewobstal@gmail.com>
2026-08-05 16:39:48 +0300

    updating the code to add result controller

--- 1.py
+++ 1.py
@@ -1,25 +1,42 @@
 name=input("Enter your name ")
-result1=float(input("Enter result for subject 1 "))
-result2=float(input("Enter result for subject 2 "))
-result3=float(input("Enter result for subject 3 "))
+
+while True:
+    result1 = float(input("Enter result for subject 1: "))
+    if 0 <= result1 <= 100:
+        break
+    print("Error! Result must be between 0 and 100.")
+
+while True:
+    result2 = float(input("Enter result for subject 2: "))
+    if 0 <= result2 <= 100:
+        break
+    print("Error! Result must be between 0 and 100.")
+
+while True:
+    result3 = float(input("Enter result for subject 3: "))
+    if 0 <= result3 <= 100:
+        break
+    print("Error! Result must be between 0 and 100.")
+
+
 average=(result1+result2+result3)/3
 if average>=85:
-    grade="A"
+        grade="A"
 elif average>=70:
-    grade="B"
+        grade="B"
 elif average>=60:
-    grade="C"
+        grade="C"
 elif average>=50:
-    grade="D"
+        grade="D"
 else:
-    grade="F"
+        grade="F"
     
 if average>=50:
-    result="Pass"
+        result="Pass"
 else:
-    result="Fail"
+        result="Fail"
     
-print("Your status", name)
+print("Your result", name)
 print("Your average mark is", average)
 print("Your grade is ", grade)
 print("your status is",result)
